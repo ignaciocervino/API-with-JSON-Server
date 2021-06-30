3425c95d6f51212f682fadf66160c6cf701ac703
ignaciocervino <ignaciocervino@gmail.com>
2021-06-30 14:57:23 -0300

    #08 Display clients from API

--- js/API.js
+++ js/API.js
@@ -1,5 +1,5 @@
 const url= 'http://localhost:4000/clientes';
-
+//Cuando se crea nuevo cliente
 export const nuevoCliente = async (cliente)=>{
     try {
         await fetch(url,{
@@ -11,6 +11,16 @@ export const nuevoCliente = async (cliente)=>{
 
         });
         window.location.href = 'index.html';
+    } catch (error) {
+        console.log(error);
+    }
+}
+//Obtiene todos los clientes
+export const obtenerClientes = async ()=>{
+    try {
+        const resultado = await fetch(url);
+        const clientes = await resultado.json();
+        return clientes;
     } catch (error) {
         console.log(error);
     }
