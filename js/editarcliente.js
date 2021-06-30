a6b4b0641876df6e16431917c804833491ee672f
ignaciocervino <ignaciocervino@gmail.com>
2021-06-30 15:27:53 -0300

    #11 Save changes when modify client

--- js/editarcliente.js
+++ js/editarcliente.js
@@ -1,5 +1,5 @@
-import {obtenerCliente} from './API.js';
-
+import {obtenerCliente,editarCliente} from './API.js';
+import {mostrarAlerta,validar} from './funciones.js';
 (function(){
     //campos del formulario
     const nombreInput = document.querySelector('#nombre');
@@ -14,6 +14,10 @@ import {obtenerCliente} from './API.js';
 
         const cliente = await obtenerCliente(idCliente);
         mostrarCliente(cliente);
+
+        //Submit al formulario
+        const formulario = document.querySelector('#formulario');
+        formulario.addEventListener('submit',validarCliente);
     });
 
     function mostrarCliente(cliente){
@@ -23,6 +27,26 @@ import {obtenerCliente} from './API.js';
         emailInput.value = email;
         telefonoInput.value = telefono;
         idInput.value = id;
+    }
+
+    function validarCliente(e){
+        e.preventDefault();
+        const cliente={
+            nombre: nombreInput.value,
+            email: emailInput.value,
+            telefono: telefonoInput.value,
+            empresa: empresaInput.value,
+            id: parseInt(idInput.value)
+        }
+
+        if (validar(cliente)) {
+            //Mostrar mensaje
+            mostrarAlerta('Todos los campos son obligatorios');
+            return;
+        }
+
+        //Reescribe el objeto
+        editarCliente(cliente);
     }
 
 })();
